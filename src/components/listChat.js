d0725fac6526ecdd796940e998b3343689e743b8
Orkanen <Filip.v.antonsson@gmail.com>
2022-09-21 09:55:47 +0200

    4.0.5
    "Code" no longer needs to be defined from server sockets.
    Fixed styling for editor.

--- src/components/listChat.js
+++ src/components/listChat.js
@@ -2,9 +2,14 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import "../css/listchat.css";
 
 export default function ListChat({item, code, name, message}) {
+    var temp = "success";
+
+    if (code == "primary") {
+        temp = "primary";
+    }
     return (
         <ListGroup className="">
-            <ListGroup.Item className={"card-"+code} key={item} variant={code}>
+            <ListGroup.Item className={"card-"+temp} key={item} variant={code}>
                 {name +" : "+ message}
             </ListGroup.Item>
         </ListGroup>
